84946585e4648fba8134e359951f20160d7bdd36
ibetter825 <y825y@qq.com>
2017-05-18 18:59:33 +0800

    ss

--- src/test/java/com/tktick/service/TestArticleService.java
+++ src/test/java/com/tktick/service/TestArticleService.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Service;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import com.tktick.Application;
@@ -18,6 +19,7 @@ import com.tktick.dao.repository.TkArticleRepository;
 @RunWith(SpringJUnit4ClassRunner.class) // SpringJUnit支持，由此引入Spring-Test框架支持！ 
 @SpringBootTest(classes = Application.class) // 指定SpringBoot工程的Application启动类
 @WebAppConfiguration // 由于是Web项目，Junit需要模拟ServletContext，因此我们需要给我们的测试类加上@WebAppConfiguration。
+@Service
 public class TestArticleService{
 	
 	@Autowired
