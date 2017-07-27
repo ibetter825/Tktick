8cdeead0c5486451ea401688888393d399d019fe
ibetter825 <y825y@qq.com>
2017-07-28 00:38:13 +0800

    上传图片

--- src/main/java/com/tktick/controller/web/CommonController.java
+++ src/main/java/com/tktick/controller/web/CommonController.java
@@ -4,11 +4,16 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import com.tktick.bean.constant.CommonConstant;
+import com.tktick.bean.model.WangModel;
 import com.tktick.configure.properties.CustomPropertiesConfig;
+import com.tktick.utils.FileUtil;
+import com.tktick.utils.StringUtil;
 
 /**
  * 首页控制器
@@ -19,15 +24,25 @@ import com.tktick.configure.properties.CustomPropertiesConfig;
 @RequestMapping("/common")
 public class CommonController extends WebBaseController {
 	
-	//http://blog.csdn.net/kilua_way/article/details/54601195 SpringBoot jar上传文件问题
-	@RequestMapping("/upload")
-	public String upload(MultipartFile file){
+	//
+	/**
+	 * http://blog.csdn.net/kilua_way/article/details/54601195 SpringBoot jar上传文件问题 
+	 * @param file 上传的文件
+	 * @param from 上传的来源 eg:"editor"
+	 * @return
+	 */
+	@RequestMapping("/upload/{from}.json")
+	public Object upload(MultipartFile file, @PathVariable("from") String from){
 		String root = CustomPropertiesConfig.getProperty(CommonConstant.UPLOAD_ROOT_PATH);
+		InputStream in = null;
+		FileOutputStream out = null;
 		try {
-			InputStream in = file.getInputStream();
-			File saveFile = new File(root);
-			if(!saveFile.exists()) saveFile.mkdirs();
-			FileOutputStream out = new FileOutputStream(new File(root+"/11.jpg"));
+			in = file.getInputStream();
+			File folder = new File(root);
+			if(!folder.exists()) folder.mkdirs();
+			String savedFileName = StringUtil.uuid(false) + "." + FileUtil.getExtensionName(file.getOriginalFilename());
+			String savedFilePath = root+File.separator+savedFileName;
+			out = new FileOutputStream(new File(savedFilePath));
 			byte[] temp = new byte[1024];
 			int i = in.read(temp);
 			while (i != -1){
@@ -35,8 +50,23 @@ public class CommonController extends WebBaseController {
 				out.flush();
 				i = in.read(temp);
 			}
+			if("editor".equalsIgnoreCase(from)){
+				//返回的地址为 upload/***.jpg
+				String returnFilePath = File.separator + root.substring(root.lastIndexOf("/") + 1) + File.separator + savedFileName;
+				return new WangModel().setData(new String[]{returnFilePath});
+			}else
+				return null;
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			if(in != null){
+				try {
+					in.close();
+					if(out != null) out.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 		return null;
 	}
--- src/main/java/com/tktick/utils/FileUtil.java
+++ src/main/java/com/tktick/utils/FileUtil.java
@@ -353,4 +353,19 @@ public class FileUtil {
         ret.add(allFileName);  
         return ret;  
     }
+    /* 
+     * Java文件操作 获取文件扩展名 
+     * 
+     *  Created on: 2011-8-2 
+     *      Author: blueeagle 
+     */  
+    public static String getExtensionName(String filename) {   
+        if ((filename != null) && (filename.length() > 0)) {   
+            int dot = filename.lastIndexOf('.');   
+            if ((dot >-1) && (dot < (filename.length() - 1))) {   
+                return filename.substring(dot + 1);   
+            }   
+        }   
+        return filename;   
+    }
 }
